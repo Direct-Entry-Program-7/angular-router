17b83ba57342bd9ee80aa45e3c7c933d6de5bdbb
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-10-28 18:04:01 +0530

    Initialize BreadCrumb

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute, NavigationEnd, Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  currentRoute = 'Dashboard';
 
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd){
+        switch(event.urlAfterRedirects){
+          case "/dashboard":
+            this.currentRoute = 'Dashboard'
+            break;
+          case "/manage-customers":
+            this.currentRoute= 'Manage Customers';
+            break;
+          case "/manage-items":
+            this.currentRoute = 'Manage Items';
+            break;
+          case "/place-order":
+            this.currentRoute = 'Place Order';
+            break;
+          case "/search-orders":
+            this.currentRoute = 'Search Orders';
+            break;
+          default:
+            this.currentRoute = "Not Found";
+            break;
+        }
+      }
+    });
+  }
 }
