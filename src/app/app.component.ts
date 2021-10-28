b24d28b86247672387ac9bdcf06a087bb6405aec
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-10-28 18:30:56 +0530

    Add a deactivate guard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
 
   constructor(private router: Router) {
     this.router.events.subscribe(event => {
+
       if (event instanceof NavigationEnd){
         switch(event.urlAfterRedirects){
           case "/dashboard":
